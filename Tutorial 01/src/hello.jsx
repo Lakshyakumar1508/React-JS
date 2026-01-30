f8d6e182204216a8e2e9e62b004f69d4cd1c7f1f
Lakshya Kumar <lakshyakumar1508@gmail.com>
2026-01-30 14:23:00 +0530

    first commit

---
+++ Tutorial 01/src/hello.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const hello=() =>{
+    return (
+    //empty tags - emmet abbrivation
+    <>
+      <div>
+        <h id="id"> Hello</h>
+
+      </div>
+    </>
+    )
+}
+
+export default hello
